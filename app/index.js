2b0dcb2a443830fe04ad39c2ae79ffcf62aa0ddd
Jonathan Dunlap <jdunlap@outlook.com>
2015-12-27 18:09:49 -0800

    First working version

---
+++ app/index.js
@@ -0,0 +1,77 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.red('webpack-es6-server') + ' generator!'
+    ));
+
+    var prompts = [{
+      type    : 'input',
+      name    : 'projectName',
+      message : 'Project name',
+      default : this.appname // Default to current folder name
+    },
+    {
+      type    : 'input',
+      name    : 'projectDescription',
+      message : 'Project description'
+    },
+    {
+      type    : 'input',
+      name    : 'authorName',
+      message : 'Author name'
+    },
+    {
+      type    : 'input',
+      name    : 'authorEmail',
+      message : 'Author email'
+    },
+    {
+      type    : 'input',
+      name    : 'license',
+      message : 'License',
+      default : 'ISC'
+    }
+    ];
+
+    this.prompt(prompts, function (props) {
+      this.props = props;
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      [
+        {src: '_package.json', dst: 'package.json'},
+        {src: 'webpack.config.js', dst: 'webpack.config.js'},
+        {src: 'src/index.js', dst: 'src/index.js'},
+        {src: 'dist/app.js', dst: 'dist/app.js'}
+      ].forEach(function (f) {
+        this.fs.copyTpl(this.templatePath(f.src), this.destinationPath(f.dst), this.props);
+      }.bind(this));
+    },
+
+    projectfiles: function () {
+      var dotfiles = ['editorconfig', 'jshintrc', 'jscsrc', 'gitignore'];
+      dotfiles.forEach(function (dotfile) {
+        this.fs.copy(this.templatePath(dotfile), this.destinationPath('.' + dotfile));
+      }.bind(this));
+    }
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
---
+++ test/test-app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var path = require('path');
+var assert = require('yeoman-generator').assert;
+var helpers = require('yeoman-generator').test;
+var os = require('os');
+//var mocha = require('mocha'); // placeholder
+
+describe('webpack-es6-server:app', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../app'))
+      .withOptions({ skipInstall: true })
+      .withPrompts({
+        projectName: 'my-project',
+        projectDescription: 'my project description',
+        authorName: 'Jonathan Dunlap',
+        authorEmail: 'jdunlap@outlook.com',
+        license: 'Apache 2.0'
+      })
+      .on('end', done);
+  });
+
+  it('creates files', function () {
+    assert.file([
+      'package.json',
+      '.editorconfig',
+      '.jshintrc',
+      '.jscsrc',
+      '.gitignore',
+      'webpack.config.js',
+      'dist/app.js',
+      'src/index.js'
+    ]);
+  });
+
+  it('templates metadata in package.json', function () {
+    assert.fileContent([
+      ['package.json', /"name": "my-project",/],
+      ['package.json', /"description": "my project description",/],
+      ['package.json', /"author": "Jonathan Dunlap <jdunlap@outlook.com>",/],
+      ['package.json', /"license": "Apache 2.0",/]
+    ]);
+  })
+});
